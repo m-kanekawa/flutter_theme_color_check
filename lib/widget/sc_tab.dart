f33efea23768a78005468e0852d2f9d9cf6c1ec4
m-kanekawa <account@kanekawa.dev>
2026-06-25 22:44:14 +0200

    show NavigationBar only TabComponent selected

--- lib/widget/sc_tab.dart
+++ lib/widget/sc_tab.dart
@@ -1,54 +1,81 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_theme_color_check/widget/tab_color.dart';
 import 'package:flutter_theme_color_check/widget/tab_component.dart';
 import 'package:flutter_theme_color_check/widget/tab_pick.dart';
 
-class ScTab extends HookConsumerWidget {
+class ScTab extends StatefulWidget {
   const ScTab({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    int currentPageIndex = 0;
-    // final modeN = ref.read(screenModeProvider.notifier);
+  ScTabState createState() => ScTabState();
+}
 
-    return DefaultTabController(
-      initialIndex: 0,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text("Theme Color Checker"),
-          bottom: const TabBar(
-            tabs: <Widget>[
-              Tab(text: 'pick', icon: Icon(Icons.cloud_outlined)),
-              Tab(text: 'color', icon: Icon(Icons.beach_access_sharp)),
-              Tab(text: 'component', icon: Icon(Icons.brightness_5_sharp)),
-            ],
-          ),
-        ),
-        body: TabBarView(
-          children: <Widget>[TabPick(), TabColor(), TabComponent()],
-        ),
-        bottomNavigationBar: NavigationBar(
-          onDestinationSelected: (int index) {},
-          selectedIndex: currentPageIndex,
-          destinations: const <Widget>[
-            NavigationDestination(
-              selectedIcon: Icon(Icons.home),
-              icon: Icon(Icons.home_outlined),
-              label: 'Home',
-            ),
-            NavigationDestination(
-              icon: Badge(child: Icon(Icons.notifications_sharp)),
-              label: 'Notifications',
-            ),
-            NavigationDestination(
-              icon: Badge(label: Text('2'), child: Icon(Icons.messenger_sharp)),
-              label: 'Messages',
-            ),
+class ScTabState extends State<ScTab> with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    _tabController.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    int currentPageIndex = 0;
+
+    debugPrint("index = ${_tabController.index}");
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Theme Color Checker"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: <Widget>[
+            Tab(text: 'pick', icon: Icon(Icons.cloud_outlined)),
+            Tab(text: 'color', icon: Icon(Icons.beach_access_sharp)),
+            Tab(text: 'component', icon: Icon(Icons.brightness_5_sharp)),
           ],
         ),
       ),
+
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[TabPick(), TabColor(), TabComponent()],
+      ),
+
+      bottomNavigationBar: (_tabController.index == 2)
+          ? NavigationBar(
+              onDestinationSelected: (int index) {},
+              selectedIndex: currentPageIndex,
+              destinations: const <Widget>[
+                NavigationDestination(
+                  selectedIcon: Icon(Icons.home),
+                  icon: Icon(Icons.home_outlined),
+                  label: 'Home',
+                ),
+                NavigationDestination(
+                  icon: Badge(child: Icon(Icons.notifications_sharp)),
+                  label: 'Notifications',
+                ),
+                NavigationDestination(
+                  icon: Badge(
+                    label: Text('2'),
+                    child: Icon(Icons.messenger_sharp),
+                  ),
+                  label: 'Messages',
+                ),
+              ],
+            )
+          : null,
     );
   }
 }
